75ad540596d94a20787cf394f19dc796272b9d48
makkenzo <nekgo2009@gmail.com>
2025-02-10 17:12:18 +0500

    Implements custom Gilroy font
    Adds the Gilroy font family with various weights (Regular, SemiBold, Bold, Heavy, Black) to the project.
    
    Configures the application to use Gilroy as the default font for sans-serif, headings, and body text.
    
    Removes the dependency on the 'next/font/google' package and the associated Geist fonts.

---
+++ src/app/components/page.tsx
@@ -0,0 +1,23 @@
+const Components = () => {
+    return (
+        <div className="grid grid-cols-2 gap-8 p-8">
+            <div className="border p-4">
+                <h1 className="text-xl">Съешь еще этих французских булок, да выпей же чаю.</h1>
+                <h2 className="text-lg">Съешь еще этих французских булок, да выпей же чаю.</h2>
+                <h3 className="text-base">Съешь еще этих французских булок, да выпей же чаю.</h3>
+                <h4 className="text-sm">Съешь еще этих французских булок, да выпей же чаю.</h4>
+                <h5 className="text-xs">Съешь еще этих французских булок, да выпей же чаю.</h5>
+            </div>
+            <div className="border p-4">
+                <h1 className="text-xl font-bold">Съешь еще этих французских булок, да выпей же чаю.</h1>
+                <h2 className="text-lg font-bold">Съешь еще этих французских булок, да выпей же чаю.</h2>
+                <h3 className="text-base font-bold">Съешь еще этих французских булок, да выпей же чаю.</h3>
+                <h4 className="text-sm font-bold">Съешь еще этих французских булок, да выпей же чаю.</h4>
+                <h5 className="text-xs font-bold">Съешь еще этих французских булок, да выпей же чаю.</h5>
+            </div>
+        </div>
+    );
+};
+
+export default Components;
+
