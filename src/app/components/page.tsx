5631305212814a6254d37e931ec05a9dba132862
makkenzo <nekgo2009@gmail.com>
2025-02-11 13:44:17 +0500

    Enhances UI components and color palette
    Updates button component with new variants and sizes for improved user interface
    Introduces an input component for consistent text field styling
    Adjusts color palette values for primary, secondary, destructive, accent, warning, and success hues
    
    Adds Button and Input components to UI library
    Refsactors global CSS for enhanced color customization

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -1,6 +1,9 @@
+import { Button } from '@/components/ui/button';
+import { RiProgress5Line } from 'react-icons/ri';
+
 const Components = () => {
     return (
-        <div className="grid grid-cols-2 gap-8 p-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 p-8">
             <div className="border p-4">
                 <h1 className="text-xl">Съешь еще этих французских булок, да выпей же чаю.</h1>
                 <h2 className="text-lg">Съешь еще этих французских булок, да выпей же чаю.</h2>
@@ -14,6 +17,20 @@ const Components = () => {
                 <h3 className="text-base font-bold">Съешь еще этих французских булок, да выпей же чаю.</h3>
                 <h4 className="text-sm font-bold">Съешь еще этих французских булок, да выпей же чаю.</h4>
                 <h5 className="text-xs font-bold">Съешь еще этих французских булок, да выпей же чаю.</h5>
+            </div>
+            <div className="border p-4 flex gap-4 items-center">
+                <Button size="lg">
+                    <RiProgress5Line className="size-[26px]" />
+                    Large button
+                </Button>
+                <Button size="md">
+                    <RiProgress5Line />
+                    Medium button
+                </Button>
+                <Button size="sm">
+                    <RiProgress5Line />
+                    Small button
+                </Button>
             </div>
         </div>
     );
