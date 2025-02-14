d72901f1d6846fc0de825259e347ab3416a008f3
makkenzo <nekgo2009@gmail.com>
2025-02-14 13:51:39 +0500

    Adds footer and updates styling
    Adds a footer component to the main layout, improving the overall user experience.  Updates the global CSS with new color variables and styling adjustments for improved visual consistency.  Also updates the logo component and several icons.

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/navigation/footer';
 import Header from '@/components/navigation/header';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -6,6 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             <div className="relative flex h-dvh max-h-dvh w-screen max-w-[100dvw] flex-col overflow-y-auto overflow-x-hidden bg-background-contrast dark:bg-background">
                 <Header />
                 <div className="flex-1">{children}</div>
+                <Footer />
             </div>
         </>
     );
--- src/components/navigation/footer.tsx
+++ src/components/navigation/footer.tsx
@@ -1,9 +1,61 @@
+import { FaTelegramPlane } from 'react-icons/fa';
+
+import Link from 'next/link';
+
+import Logo from '../reusable/logo';
+
 interface FooterProps {}
 
 const Footer = ({}: FooterProps) => {
     return (
-        <div>
-            <h1>Footer</h1>
+        <div className="bg-surface-dark max-h-[302px] h-full flex flex-col justify-between">
+            <div className="max-w-[1208px] grid grid-cols-4 mx-auto xl:px-0 px-8 pt-[40px] w-full">
+                <Logo whiteText />
+                <ul className="text-white space-y-4">
+                    <li className="text-lg">
+                        <Link href={'/'}>Веб-программирование</Link>
+                    </li>
+                    <li>
+                        <Link href={'/'}>Разработка мобильных приложений</Link>
+                    </li>
+                    <li>
+                        <Link href={'/'}>Java: Новичок</Link>
+                    </li>
+                    <li>
+                        <Link href={'/'}>PHP: Новичок</Link>
+                    </li>
+                </ul>
+                <ul className="text-white space-y-4">
+                    <li className="text-lg">
+                        <Link href={'/'}>Adobe Illustrator</Link>
+                    </li>
+                    <li>
+                        <Link href={'/'}>Adobe Photoshop</Link>
+                    </li>
+                    <li>
+                        <Link href={'/'}>Design Logo</Link>
+                    </li>
+                </ul>
+                <ul className="text-white space-y-4">
+                    <li className="text-lg">
+                        <Link href={'/'}>Написание курса</Link>
+                    </li>
+                    <li>
+                        <Link href={'/'}>Фото-редактирование</Link>
+                    </li>
+                    <li>
+                        <Link href={'/'}>Монтаж видео</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="max-w-[1208px] flex justify-between items-center mx-auto text-white-90 pb-4 w-full">
+                <p>Copyright © {new Date().getFullYear()} Code Horizon. All rights reserved.</p>
+                <div className="flex items-center gap-4">
+                    <Link href={'https://t.me/makkenz0'} className="group">
+                        <FaTelegramPlane className="size-[20px] text-white-90 group:hover:text-white" />
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
