5e1db988c053930cb927696f1247e1a63aa69744
makkenzo <nekgo2009@gmail.com>
2025-05-21 17:59:12 +0500

    chore: update sitemap change frequency to daily
    - Change the changeFrequency property in the sitemap configuration from weekly to daily for the base URL and courses page.
    - This ensures search engines crawl the site more frequently, reflecting more up-to-date content.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -7,13 +7,13 @@ export default function sitemap(): MetadataRoute.Sitemap {
         {
             url: BASE_URL,
             lastModified: new Date(),
-            changeFrequency: 'weekly',
+            changeFrequency: 'daily',
             priority: 1,
         },
         {
             url: `${BASE_URL}/courses`,
             lastModified: new Date(),
-            changeFrequency: 'weekly',
+            changeFrequency: 'daily',
             priority: 0.9,
         },
         {
